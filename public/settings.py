b62e86675ad50a748e2a5079d30c2b09c1fd4de1
Paul Tagliamonte <tag@pault.ag>
2015-06-13 23:35:51 -0400

    foo

--- public/settings.py
+++ public/settings.py
@@ -1,6 +1,6 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "foo")
 
 DEBUG = True
 ALLOWED_HOSTS = []
